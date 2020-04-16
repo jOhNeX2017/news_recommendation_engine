16118cbaff7c4c25cdccaaa9a1e6fd217288ac2a
jOhNeX2017 <rajatkesarwani2011@gmail.com>
2020-04-16 18:28:09 +0530

    news datset from kaggle

--- news.py
+++ news.py
@@ -1,3 +1,10 @@
 import pandas as pd
-
+import uuid
 data =  pd.read_csv('dataset.csv')
+data['id']='abc'
+for i in range(0,len(data)):
+    data['id'][i]=uuid.uuid4()
+
+print(data.shape())
+
+data.to_csv('dataset.csv')
