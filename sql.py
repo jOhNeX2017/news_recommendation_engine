3b3c10c130898888477d5fb3176f3dc2b7071124
jOhNeX2017 <rajatkesarwani2011@gmail.com>
2020-04-30 12:22:15 +0530

    uploads the data from python to mysql

--- sql.py
+++ sql.py
@@ -1,5 +1,17 @@
 from sqlalchemy import create_engine
 import pandas as pd
+from zipfile import ZipFile
+import os
+
+
+
+zip_filename = 'news-category-dataset.zip'
+
+with ZipFile(zip_filename, 'r') as zip:
+    zip.extractall()
+
+df = pd.read_json('News_Category_Dataset_v2.json',lines=True)
+df.to_csv('data.csv')
 
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(user="root",pw="",db="recommend"), pool_size=10, max_overflow=20)
 
@@ -13,3 +25,7 @@ df = df.dropna(how='any')
 
 # this will load the csv file in the sql
 df.to_sql('data',con = engine, if_exists = 'append', chunksize = 100000)
+
+os.remove('News_Category_Dataset_v2.json')
+#removed the converted CSV file 
+os.remove('data.csv')
